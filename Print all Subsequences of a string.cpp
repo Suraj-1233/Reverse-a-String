d528ac106e8bca9c524183d02e9686e1df98c217
suraj kannujiya <surajkannujiya517@gmail.com>
2022-01-15 13:56:32 +0530

    substring

---
+++ Print all Subsequences of a string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+
+  string s;
+  cin>>s;
+   for(int i=0;i<s.size();i++)
+     {
+         for(int j=i;j<s.size();j++)
+         {
+             cout<<s.substr(i,j-i+1)<<" "<<endl;;
+         }
+     }
+
+
+ }
