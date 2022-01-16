ad9c6fc611fec76c84a15a5904dbc44b503e795c
suraj kannujiya <surajkannujiya517@gmail.com>
2022-01-16 15:04:22 +0530

    mobile

---
+++ Convert a Sentence into its equivalent mobile numeric keypad sequence..cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+   string s;
+   cin>>s;
+   
+
+string str[]={"2","22","222","3", "33","333","4","44","444","5","55","555","6","66" ,"666","7","77","777","7777","8","88","888","9","99","999","9999"};
+  string ans=" ";
+  for(int i=0;i<s.size();i++)
+  {
+      if(s[i]==' ')
+      ans+='0';
+      else
+      ans+=str[s[i]-65];
+  }
+     cout<<ans<<endl;    
+
+}
